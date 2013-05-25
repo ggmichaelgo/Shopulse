2a24fc92690251ff32ecf6ad0d2f8891ac9cf177
Michael Go <ggmichaelgo@gmail.com>
2013-05-24 23:08:46 -0300

    Fix migration

--- db/migrate/20130521173703_retailer.rb
+++ db/migrate/20130521173703_retailer.rb
@@ -5,12 +5,12 @@ class Retailer < ActiveRecord::Migration
 			CREATE TABLE retailers  as select * from role_properties
 		SQL
 
-		execute <<-SQL
-			ALTER TABLE retailers DROP id;
-		SQL
-		execute <<-SQL
-			ALTER TABLE retailers ADD id int(10) PRIMARY KEY AUTO_INCREMENT;
-		SQL
+		# execute <<-SQL
+		# 	ALTER TABLE retailers DROP id;
+		# SQL
+		# execute <<-SQL
+		# 	ALTER TABLE retailers ADD id int(10) PRIMARY KEY AUTO_INCREMENT;
+		# SQL
 		
 		add_column :retailers, :name, :string
 		# add_column :retailers, :outputs, :text
